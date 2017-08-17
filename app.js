5be1500669b768c71e89bc4ea0a98f5546aa99ae
martiuslim <martiuslim@outlook.com>
2017-08-17 23:06:40 +0800

    added more bot convos for vivace

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ require('dotenv').config();
 const builder = require('botbuilder');
 const emoji = require('node-emoji');
 const express = require('express');
-const app = express();
-let connector;
+const firebase = require('firebase');
 
-// Setup bot connector and server
-connector = new builder.ChatConnector({
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET
+};
+const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
+firebase.initializeApp(config);
+const app = express();
+const db = firebase.database();
+
+// Setup bot connector and server
 app.listen(process.env.PORT, function() {
   console.log("%s started and listening on port %s..", 'mspbot', this.address().port); 
 });
@@ -18,27 +28,31 @@ app.post('/api/messages', connector.listen());
 const bot = new builder.UniversalBot(connector);
 bot.dialog('/', (session) => {
   switch(session.message.text) {
-    case "/start": {
+    case "/start" || '/start@smumspbot': {
       session.beginDialog('greeting');
       break;
     }
-    case "/msp": {
-      session.beginDialog('msp');
+    case "/me" || '/me@smumspbot': {
+      session.beginDialog('me');
       break;
     }
-    case "/fortunecookie": {
-      session.beginDialog('fortunecookie');
-      break;
-    }
-    case "/games": {
-      session.beginDialog('games');
-      break;
-    }
-    case "/about": {
+    case "/about" || '/about@smumspbot': {
       session.beginDialog('about');
       break;
     }
-    case "/help": {
+    case "/dotnet" || '/dotnet@smumspbot': {
+      session.beginDialog('dotnet');
+      break;
+    }
+    case "/fortunecookie" || '/fortunecookie@smumspbot': {
+      session.beginDialog('fortunecookie');
+      break;
+    }
+    case "/games" || '/games@smumspbot': {
+      session.beginDialog('games');
+      break;
+    }
+    case "/help" || '/help@smumspbot': {
       session.beginDialog('help');
       break;
     }
@@ -49,11 +63,67 @@ bot.dialog('/', (session) => {
 });
 
 bot.dialog('greeting', (session, args) => {
-  session.endDialog("Hello! I am DotNetBot %s, your friendly neighbourhood chatbot built for the SMU Microsoft Student Community.  \nSend '/msp' to learn more about what DotNet Society and our Microsoft Student Partners do or '/help' to see what else I can do!", emoji.get('robot_face'));
+  session.endDialog("Hello! I am **DotNetBot** %s, your friendly neighbourhood chatbot built for the **SMU Microsoft Student Community**.  \n\nSend */dotnet* to learn more about what DotNet Society and our Microsoft Student Partners do or */help* to see what else I can do!", emoji.get('robot_face'));
 });
 
-bot.dialog('msp', (session, args) => {
-  session.endDialog("Feature in progress! Stay tuned..");
+bot.dialog('me', [
+  (session, args) => {
+    if (!session.userData.name) {
+      builder.Prompts.choice(session, "Hi there and nice to meet you! I am **DotNetBot** and I want to get to know you better :D  \n", "sure!|not right now.. ):");
+    } else {
+      session.send(
+        "Hi again %s!  \n\nYour current profile is:  \nName: %s  \nEmail: %s  \nFaculty: %s  \nYear Of Study: %i", 
+        session.userData.name, 
+        session.userData.name,
+        session.userData.email, 
+        session.userData.faculty, 
+        session.userData.yearOfStudy
+      );
+      builder.Prompts.confirm(session, "Would you like to change your details?  \n(Reply with 'yes' or 'no')");
+    }
+  },
+  (session, results) => {
+    results.response.index == 1 || results.response ? session.beginDialog('profile') : session.endDialog("I'll see you around then!");
+  }
+]);
+
+bot.dialog('profile', [
+  (session, args) => {
+    session.send("Nice! I'll be asking you a series of questions. All you have to do is to reply me like you would to someone else and we're good!");
+    builder.Prompts.text(session, "What's your name?");
+  },
+  (session, results) => {
+    session.userData.name = results.response;
+    builder.Prompts.text(session, "Hi " + session.userData.name + "! Next, what's your SMU email address?");
+  },
+  (session, results) => {
+    session.userData.email = results.response;
+    builder.Prompts.text(session, "Wow " + session.userData.email + "! I wish I had thought of that first! What are you studying in SMU?");
+  },
+  (session, results) => {
+    session.userData.faculty = results.response;
+    builder.Prompts.number(session, "Cool! Maybe one day I'll be as smart as you! What year of study are you currently at?");
+  },
+  (session, results) => {
+    session.userData.yearOfStudy = results.response;
+    session.send(
+      "Great! Here's your information:  \nName: %s  \nEmail: %s  \nFaculty: %s  \nYear Of Study: %i", 
+      session.userData.name, 
+      session.userData.email, 
+      session.userData.faculty, 
+      session.userData.yearOfStudy
+    );
+    session.endDialog("Ping me again if I got your details wrongly %s", emoji.get('innocent'));
+  }
+]);
+
+bot.dialog('about', (session, args) => {
+  session.endDialog("Hi there! I'm **DotNetBot**, a chatbot built for the **SMU Microsoft Student Community (SMU DotNet Society)** by our friendly *Microsoft Student Partners*. I'm still young and learning how to do new things!  \n\nIf you find any bugs or have any feedback to help me improve, please let @martiuslim know and he will make me better! Thank you %s", emoji.get('smile'));
+});
+
+bot.dialog('dotnet', (session, args) => {
+  session.send("Hey! We are **DotNet Society** and we are mainly a bunch of technology enthusiasts with a passion for Microsoft Technology %s.  \n\nOur friendly **Microsoft Student Partners** conduct workshops and host events so that we can all learn and grow together!  \n\nIf **learning new technologies** and **building cool stuff** sounds fun to you, or if you want to become a Microsoft Student Partner yourself, please feel free to join us and spread the word!", emoji.get('v'));
+  session.endDialog("We'd love you to join us at our **SMU Microsoft Student Community** here: %s", 'https://t.me/joinchat/BTX9YUMEqizbHZeYUhNdPA');
 });
 
 bot.dialog('fortunecookie', (session, args) => {
@@ -61,22 +131,35 @@ bot.dialog('fortunecookie', (session, args) => {
   session.endDialog(fortunes);
 });
 
-bot.dialog('games', (session, args) => {
-  session.endDialog("Feature in progress! Stay tuned..");
-});
-
-bot.dialog('about', (session, args) => {
-  session.endDialog("Hi there! I'm DotNetBot, a chatbot built for the SMU Microsoft Student Community (SMU DotNet Society) by our friendly Microsoft Student Partners.  \nI'm still young and learning how to do new things!  \nIf you have any feedback or suggestions to help me improve, please let @martiuslim know and he will make me better! Thank you %s", emoji.get('smile'));
-});
+bot.dialog('games', [
+  (session, args) => {
+    builder.Prompts.choice(session, "Let's play a game!  \n", 'Flip a Coin|Roll Dice|Quit');
+  }, 
+  (session, results) => {
+    switch (results.response.index) {
+      case 0: {
+        session.endDialog("*Flipping a coin..*  \nAh! I have not learnt how to do that yet. Check back again soon!");
+        break;
+      }
+      case 1: {
+        session.endDialog("*Rolling some dice..*  \nAh! I have not learnt how to do that yet. Check back again soon!");
+        break;
+      }
+      default: {
+        session.endDialog("Thanks for playin'! See you around %s", emoji.get('grin'));
+      }
+    }
+  }
+]);
 
 bot.dialog('help', (session, args) => {
   session.endDialog("I'm glad you asked! Here's a list of what I can do, just click on any of them to get started %s  \n%s  \n%s  \n%s  \n%s  \n%s  ", 
     emoji.get('muscle'),
-    '/msp',
-    '/fortunecookie',
-    '/games', 
-    '/about', 
-    '/help'
+    '/me (help me get to know you better!)',
+    '/about (learn more about me and how you can help make me better!)',
+    '/dotnet (learn more about DotNet Society and our SMU Microsoft Student Community!)',
+    '/fortunecookie (get your fortune.. after I eat your cookie ' + emoji.get('smiling_imp') + ')',
+    '/games (play some simple games with me!)'
   );
 });
 
@@ -98,7 +181,7 @@ bot.on('conversationUpdate', (message) => {
 
     bot.send(new builder.Message()
       .address(message.address)
-      .text("Welcome %s to SMU's Microsoft Student Community, hooray!", membersAdded));
+      .text("Welcome %s to SMU's Microsoft Student Community, hooray! %s", membersAdded, emoji.random().emoji));
   }
 
   if (message.membersRemoved && message.membersRemoved.length > 0) {
@@ -111,7 +194,7 @@ bot.on('conversationUpdate', (message) => {
 
     bot.send(new builder.Message()
       .address(message.address)
-      .text("Goodbye " + membersRemoved + " :("));
+      .text("Goodbye %s %s", membersRemoved, emoji.get('cry')));
   }
 });
 
@@ -137,3 +220,12 @@ const fortunes = [
   "The Wheel of Good Fortune is finally turning in your direction!",
   "The eyes believe themselves; the ears believe other people."
 ];
+
+function writeUserData(userId, name, email, faculty, yearOfStudy) {
+  db.ref('users/' + userId).set({
+    name,
+    email,
+    faculty,
+    yearofStudy
+  });
+};
